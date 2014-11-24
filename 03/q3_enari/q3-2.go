fc1313772c0bf961dcb285e6d5e2b14e28cc7296
enack <road.of.reality.enack2010@gmail.com>
2014-11-24 09:09:24 +0900

    再コミット

--- 03/q3_enari/q3-2.go
+++ 03/q3_enari/q3-2.go
@@ -15,17 +15,17 @@ func main(){
 
   for cnt := 1; cnt < LIST_SIZE; cnt++ {
     primeList[cnt] = primeList[cnt - 1] + 1  // ひとつ前の要素に1加えた値を次の要素に代入
-    prime := -1
-    for prime == -1 {
-      flg := true  // primeを素数として扱う(仮定)
+    loop := -1
+    for loop == -1 {
+      flg := true  // primeList[cnt]を素数として扱う(仮定)
       for idx := 0; idx < cnt; idx++ {
         if math.Mod(float64(primeList[cnt]), float64(primeList[idx])) == 0 {
-          flg = false
+          flg = false  // primeList[cnt]を素数ではない(矛盾)
           break
         }
       }
       if flg == true {
-         prime = 0
+         loop = 0  // primeList[cnt]が素数であるためループ終了
       } else {
         primeList[cnt] = primeList[cnt] + 1
       }
